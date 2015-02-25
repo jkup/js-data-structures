e4b5ccc6a561e7ef229793dec5d0b6945b0f73b4
Jon Kuperman <jon.kuperman@gmail.com>
2015-02-24 20:43:23 -0800

    added linked list

---
+++ data-structures/linked-list/linked-list.js
@@ -0,0 +1,36 @@
+function LinkedList(){
+  this.head = null;
+}
+
+LinkedList.prototype.push = function(val){
+    var node = {
+       value: val,
+       next: null
+    }
+
+    if(!this.head){
+      this.head = node;
+    }
+
+    else{
+      current = this.head;
+      while(current.next){
+        current = current.next;
+      }
+      current.next = node;
+    }
+}
+
+/*
+ * Tests
+**/
+var linkedList = new LinkedList();
+
+//push node
+linkedList.push(2);
+linkedList.push(3);
+linkedList.push(4);
+
+console.log(linkedList.head); //Object {data: 2, next: Object}
+console.log(linkedList.head.next); //Object {data: 3, next: Object}
+console.log(linkedList.head.next.next); //Object {data: 4, next: null}
